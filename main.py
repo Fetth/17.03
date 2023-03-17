873456b4e7ea7e7049084af45d51a2a6c042438a
Fetth <kruglovegor0906@icloud.com>
2023-03-17 20:51:09 +0300

    commit wrok

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pz
+import pz2
 
-#pz.pz1()
+# pz.pz1()
 
-#pz.pz2()
+# pz.pz2()
 
 # pz.pz3()
 
 # pz.pz4()
 
+# pz.pz5()
+
+# pz.pz6()
+
+# pz2.pz2()
+
+pz2.pz3()
--- pz.py
+++ pz.py
@@ -1,48 +1,77 @@
-def pz1():
-    file = open("pz1in.txt", encoding="utf-8")
-    inStr = file.read()
-    print(inStr)
+# def pz1():
+#     file = open("pz1in.txt", encoding="utf-8")
+#     inStr = file.read()
+#     print(inStr)
 
-    fileOut = open("pz1out.txt", 'w', encoding='utf-8')
-    fileOut.write(inStr)
-
-
-def pz2():
-    filein = open("pz2in.txt", encoding="utf-8")
-    listIn = filein.readlines()
-    print(listIn)
-
-    fileOut = open("pz2out.txt", 'w', encoding='utf-8')
-    for row in listIn:
-        fileOut.write(row)
+#     fileOut = open("pz1out.txt", 'w', encoding='utf-8')
+#     fileOut.write(inStr)
 
 
-def pz3():
-    filein = open("pz3in.txt", encoding="utf-8")
-    listIn = filein.readlines()
+# def pz2():
+#     filein = open("pz2in.txt", encoding="utf-8")
+#     listIn = filein.readlines()
+#     print(listIn)
 
-    listIn.reverse()
-    print(listIn)
+#     fileOut = open("pz2out.txt", 'w', encoding='utf-8')
+#     for row in listIn:
+#         fileOut.write(row)
 
-    fileOut = open("pz3out.txt", 'w', encoding='utf-8')
-    for row in listIn:
-        fileOut.write(row)
 
-def pz4():
-    filein = open("pz4in.txt", encoding="utf-8")
-    listIn = filein.readlines()
-    indexOut = 0
-    for row in listIn:
-        for s in row:
-            if s == ',':
-                indexOut = listIn.index(row)
+# def pz3():
+#     filein = open("pz3in.txt", encoding="utf-8")
+#     listIn = filein.readlines()
+
+#     listIn.reverse()
+#     print(listIn)
+
+#     fileOut = open("pz3out.txt", 'w', encoding='utf-8')
+#     for row in listIn:
+#         fileOut.write(row)
+
+# def pz4():
+#     filein = open("pz4in.txt", encoding="utf-8")
+#     listIn = filein.readlines()
+#     indexOut = 0
+#     for row in listIn:
+#         for s in row:
+#             if s == ',':
+#                 indexOut = listIn.index(row)
     
 
-    fileOut = open("pz4out.txt", 'w', encoding='utf-8')
-    for row in listIn:
-        if listIn.index(row) == indexOut + 1:
-            rowOut = row.replace("\n", "************\n")
-            fileOut.write(rowOut)
-        else:
-            fileOut.write(row)
+#     fileOut = open("pz4out.txt", 'w', encoding='utf-8')
+#     for row in listIn:
+#         if listIn.index(row) == indexOut + 1:
+#             rowOut = row.replace("\n", "************\n")
+#             fileOut.write(rowOut)
+#         else:
+#             fileOut.write(row)
+
+
+# def pz5():
+#     file = open("pz3in.txt", encoding="utf-8")
+#     inStr = file.read()
+    
+#     sUser = input("Введите символ который хотите найти ")
+#     count = 0
+#     listIn = inStr.split(" ")
+#     for i in listIn:
+#         if i[0] == sUser:
+#             count += 1
+#     print(count)  
+
+# def pz6():
+#     file = open("pz4out.txt", encoding="utf-8")
+#     inStr = file.read()
+#     listOut = []
+#     for i in inStr:
+#         if i == "*":
+#             i = "&"
+#             listOut.append(i)
+#         elif i == "&":
+#             i = "*"
+#             listOut.append(i)
+#         else:
+#             listOut.append(i)
+#     strOut = ''.join(listOut)
+#     print(strOut)
 
